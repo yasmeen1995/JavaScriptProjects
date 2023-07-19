ad7a64db6c06f30936d32132b7348fb47da5971f
Yasmeen Mohammed <yasmeenshaik0786@gmail.com>
2023-07-18 23:54:49 -0400

    DS folder and script

--- APIWeatherApp/script.js
+++ APIWeatherApp/script.js
@@ -1,5 +1,3 @@
-
-// API_KEY for maps api
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
@@ -7,7 +5,7 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
 getWeatherData = (city) => {
-//   const URL = "https://open-weather13.p.rapidapi.com";
+  //   const URL = "https://open-weather13.p.rapidapi.com";
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
@@ -15,24 +13,24 @@ getWeatherData = (city) => {
   //CODE GOES HERE
   const weatherPromise = fetch(FULL_URL);
   return weatherPromise.then((response) => {
-    return response.json()
-  })
-}
+    return response.json();
+  });
+};
 
 /**
  * Retrieve city input and get the weather data
  * HINT: Use the promise returned from getWeatherData()
  */
 searchCity = () => {
-  const city = document.getElementById('city-input').value;
+  const city = document.getElementById("city-input").value;
   // CODE GOES HERE
   getWeatherData(city)
-  .then((data) => showWeatherData(data))
-  .catch((err) => {
-    console.log(err)
-    console.log("something went wrong")
-  })
-}
+    .then((data) => showWeatherData(data))
+    .catch((err) => {
+      console.log(err);
+      console.log("something went wrong");
+    });
+};
 
 /**
  * Show the weather data in HTML
@@ -40,13 +38,14 @@ searchCity = () => {
  */
 showWeatherData = (weatherData) => {
   //CODE GOES HERE
-  document.getElementById("city-name").innerText = weatherData.name
-  document.getElementById("weather-type").innerText = weatherData.weather[0].main
-  document.getElementById("temp").innerText = weatherData.main.temp
-  document.getElementById("min-temp").innerText = weatherData.main.temp_min
-  document.getElementById("max-temp").innerText = weatherData.main.temp_max
-  document.getElementById("weather-output").classList.add('visible');
-}
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText =
+    weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+  document.getElementById("weather-output").classList.add("visible");
+};
 
 // const url = 'https://open-weather13.p.rapidapi.com/city';
 // const options = {
